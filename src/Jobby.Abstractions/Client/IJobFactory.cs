c58037cf2a9f417512bd8317a9039f54079f240f
Vitaliy Sushkov <fornit1917@gmail.com>
2025-02-18 20:21:39 +0300

    Factory for JobModel

---
+++ src/Jobby.Abstractions/Client/IJobFactory.cs
@@ -0,0 +1,9 @@
+﻿using Jobby.Abstractions.Models;
+
+namespace Jobby.Abstractions.Client;
+
+public interface IJobFactory
+{
+    JobModel Create<TCommand>(TCommand command) where TCommand : IJobCommand;
+    JobModel Create<TCommand>(TCommand command, DateTime startTime) where TCommand : IJobCommand;
+}
---
+++ src/Jobby.Core/Client/JobFactory.cs
@@ -0,0 +1,39 @@
+﻿using Jobby.Abstractions.Client;
+using Jobby.Abstractions.CommonServices;
+using Jobby.Abstractions.Models;
+
+namespace Jobby.Core.Client;
+
+public class JobFactory : IJobFactory
+{
+    private readonly IJobParamSerializer _serializer;
+
+    public JobFactory(IJobParamSerializer serializer)
+    {
+        _serializer = serializer;
+    }
+
+    public JobModel Create<TCommand>(TCommand command) where TCommand : IJobCommand
+    {
+        return new JobModel
+        {
+            CreatedAt = DateTime.UtcNow,
+            JobName = TCommand.GetJobName(),
+            JobParam = _serializer.SerializeJobParam(command),
+            ScheduledStartAt = DateTime.UtcNow,
+            Status = JobStatus.Scheduled,
+        };
+    }
+
+    public JobModel Create<TCommand>(TCommand command, DateTime startTime) where TCommand : IJobCommand
+    {
+        return new JobModel
+        {
+            CreatedAt = DateTime.UtcNow,
+            JobName = TCommand.GetJobName(),
+            JobParam = _serializer.SerializeJobParam(command),
+            ScheduledStartAt = startTime,
+            Status = JobStatus.Scheduled,
+        };
+    }
+}
