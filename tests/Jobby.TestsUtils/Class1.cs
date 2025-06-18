a7398beb69c177a1aa1dc2ff67a58e8ca24e8f48
Vitaliy Sushkov <fornit1917@gmail.com>
2025-06-18 21:59:46 +0300

    Unit tests for SimpleJobCompletionService and JobsRegistry

--- tests/Jobby.TestsUtils/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Jobby.TestsUtils
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ tests/Jobby.TestsUtils/Jobs/TestJobCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Jobby.Core.Interfaces;
+using Jobby.Core.Models;
+
+namespace Jobby.TestsUtils.Jobs;
+
+public class TestJobCommandHandler : IJobCommandHandler<TestJobCommand>
+{
+    public Task ExecuteAsync(TestJobCommand command, JobExecutionContext ctx)
+    {
+        return Task.CompletedTask;
+    }
+}
