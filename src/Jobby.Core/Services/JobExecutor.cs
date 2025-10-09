b8aa5e5e6da0b89276156cb910fc5b4562270889
monco83 <monco@mail.ru>
2025-10-09 20:00:38 +0300

    Replace reflection by job executor (#2)
    * JobExecutionService refactoring
    
    * add unit test

---
+++ src/Jobby.Core/Services/JobExecutor.cs
@@ -0,0 +1,19 @@
+﻿using Jobby.Core.Interfaces;
+using Jobby.Core.Models;
+
+namespace Jobby.Core.Services;
+internal readonly struct JobExecutor<TCommand> : IJobExecutor
+    where TCommand : IJobCommand
+{
+    public readonly TCommand Command;
+    public readonly IJobCommandHandler<TCommand> Handler;
+
+    public JobExecutor(TCommand command, IJobCommandHandler<TCommand> handler)
+    {
+        Command = command;
+        Handler = handler;
+    }
+
+    public Task ExecuteJob(JobExecutionContext ctx)
+        => Handler.ExecuteAsync(Command, ctx);
+}
---
+++ tests/Jobby.Tests.Core/Services/JobExecutorTests.cs
@@ -0,0 +1,33 @@
+﻿using Jobby.Core.Interfaces;
+using Jobby.Core.Models;
+using Jobby.Core.Services;
+using Jobby.TestsUtils.Jobs;
+
+namespace Jobby.Tests.Core.Services;
+public class JobExecutorTests
+{
+    [Fact]
+    public async Task ExecuteJob_InvokeHandlerWithSpecifiedCommand()
+    {
+        var command = new TestJobCommand();
+        var handler = new TestJobCommandHandler();
+
+        IJobExecutor jobExecutor = new JobExecutor<TestJobCommand>(
+            command,
+            handler
+        );
+
+        var ctx = new JobExecutionContext
+        {
+            JobName = "TestJobName",
+            StartedCount = 0,
+            IsLastAttempt = false,
+            CancellationToken = CancellationToken.None,
+        };
+
+
+        await jobExecutor.ExecuteJob(ctx);
+
+        Assert.Equal(command, handler.LatestCommand);
+    }
+}
